dae31f2a7196d95f43c5e78361725fdad14a13cb
Gunther Fox <gunther.fox@gmail.com>
2014-08-11 22:48:12 -0700

    Added .gitignore and basic files

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "useless_header.h"
+
+int main()
+{
+
+std::cout << "Hello, git!" << std::endl;
+
+return 0;
+
+}
---
+++ useless_header.h
@@ -0,0 +1,5 @@
+void useless_function()
+{
+for (int i=0; i<50; i++) {}
+return;
+}
